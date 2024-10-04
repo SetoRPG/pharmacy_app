d2cffa6bf595572c829cfe8ae8239eb838fdf9c3
This PC <52000644@student.tdtu.edu.vn>
2024-10-04 12:52:20 +0700

    Update 7

--- lib/screens/home/category_screen.dart
+++ lib/screens/home/category_screen.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:pharmacy_app/controllers/medicine_controller.dart';
 
-class CategoryScreen extends StatelessWidget {
+class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
+
+  @override
+  _CategoryScreenState createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  final MedicineController _medicineController = MedicineController();
+  List<Map<String, dynamic>> _medicines = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMedicines();
+  }
+
+  Future<void> _loadMedicines() async {
+    final medicines = await _medicineController.getMedicines();
+    setState(() {
+      _medicines = medicines;
+      _isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,38 +35,40 @@ class CategoryScreen extends StatelessWidget {
         centerTitle: true,
         backgroundColor: const Color(0xFF20B6E8),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: [
-            _buildCategorySection('Thuốc giảm đau', [
-              _buildProductItem('Paracetamol', 'Thuốc giảm đau và hạ sốt',
-                  'assets/paracetamol.jpg'),
-              _buildProductItem(
-                  'Ibuprofen', 'Giảm viêm và đau', 'assets/ibuprofen.jpg'),
-            ]),
-            const SizedBox(height: 20),
-            _buildCategorySection('Vitamin & Thực phẩm chức năng', [
-              _buildProductItem(
-                  'Vitamin C', 'Tăng cường miễn dịch', 'assets/vitamin_c.jpg'),
-              _buildProductItem(
-                  'Omega-3', 'Tốt cho tim mạch', 'assets/omega3.jpg'),
-            ]),
-            const SizedBox(height: 20),
-            _buildCategorySection('Dụng cụ y tế', [
-              _buildProductItem(
-                  'Băng cá nhân', 'Bảo vệ vết thương', 'assets/bandage.jpg'),
-              _buildProductItem(
-                  'Nhiệt kế', 'Đo nhiệt độ cơ thể', 'assets/thermometer.jpg'),
-            ]),
-          ],
-        ),
-      ),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView(
+                children: _buildCategoriesWithProducts(),
+              ),
+            ),
       backgroundColor: Colors.white,
     );
   }
 
-  Widget _buildCategorySection(String categoryName, List<Widget> products) {
+  List<Widget> _buildCategoriesWithProducts() {
+    Map<String, List<Map<String, dynamic>>> categorizedMedicines = {};
+
+    for (var med in _medicines) {
+      if (categorizedMedicines.containsKey(med['medCategory'])) {
+        categorizedMedicines[med['medCategory']]!.add(med);
+      } else {
+        categorizedMedicines[med['medCategory']] = [med];
+      }
+    }
+
+    List<Widget> categoryWidgets = [];
+    categorizedMedicines.forEach((category, medicines) {
+      categoryWidgets.add(_buildCategorySection(category, medicines));
+      categoryWidgets.add(const SizedBox(height: 20));
+    });
+
+    return categoryWidgets;
+  }
+
+  Widget _buildCategorySection(
+      String categoryName, List<Map<String, dynamic>> products) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -56,21 +82,25 @@ class CategoryScreen extends StatelessWidget {
         ),
         const SizedBox(height: 10),
         Column(
-          children: products,
+          children:
+              products.map((product) => _buildProductItem(product)).toList(),
         ),
       ],
     );
   }
 
-  Widget _buildProductItem(
-      String productName, String productDescription, String imagePath) {
+  Widget _buildProductItem(Map<String, dynamic> product) {
     return Card(
       elevation: 3,
       child: ListTile(
-        leading:
-            Image.asset(imagePath, width: 50, height: 50, fit: BoxFit.cover),
-        title: Text(productName),
-        subtitle: Text(productDescription),
+        leading: Image.network(
+          _getImageUrl(product['medPrimaryImage']),
+          width: 50,
+          height: 50,
+          fit: BoxFit.cover,
+        ),
+        title: Text(product['medName']),
+        subtitle: Text(product['medIndications']),
         trailing: IconButton(
           icon: const Icon(Icons.add_shopping_cart),
           onPressed: () {
@@ -79,5 +109,16 @@ class CategoryScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  // Converts 'gs://' format to 'https://storage.googleapis.com/' format
+  String _getImageUrl(String gsUrl) {
+    const storageBaseUrl =
+        "https://storage.googleapis.com/pharmadirect-a8570.appspot.com/";
+    if (gsUrl.startsWith("gs://pharmadirect-a8570.appspot.com/")) {
+      return gsUrl.replaceFirst(
+          "gs://pharmadirect-a8570.appspot.com/", storageBaseUrl);
+    }
+    return gsUrl; // If it's already in correct format
   }
 }
